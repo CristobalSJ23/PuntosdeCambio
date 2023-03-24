0124caec3056bc3e14ddd0d4130de25e08d15084
oscar vega <oscar_rvr@outlook.com>
2023-03-24 17:47:23 -0600

    O:se cambio los input a number

--- assets/js/sistema.js
+++ assets/js/sistema.js
@@ -2,46 +2,49 @@ $(document).ready(function() {
     var obj = {};
     $('#myTable').DataTable({
         "paging": true,
-    "lengthChange": true,
-    "searching": true,
-    "ordering": true,
-    "info": true,
-    "autoWidth": true,
-        "lengthMenu": [[10,20,50, -1],[10,20,50,"Mostrar Todo"]],
-         dom: 'fr<"col-md-6 inline"l><"row"B> ti<"col-md-7 inline"p>',
+        "lengthChange": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": true,
+        "lengthMenu": [
+            [10, 20, 50, -1],
+            [10, 20, 50, "Mostrar Todo"]
+        ],
+        dom: 'fr<"col-md-6 inline"l><"row"B> ti<"col-md-7 inline"p>',
 
         buttons: [
-            
+
             {
-                extend:    'excelHtml5',
-                text:      '<i class="fa fa-file-excel-o"></i>Excel',
-                title:'Sistemas',
+                extend: 'excelHtml5',
+                text: '<i class="fa fa-file-excel-o"></i>Excel',
+                title: 'Sistemas',
                 titleAttr: 'Excel',
                 className: 'btn btn-app export excel',
                 exportOptions: {
-                    columns: [ 0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
                 }
             }
         ],
-    
+
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
         }
 
-        
+
     });
-    $('.editar').click(function(){
+    $('.editar').click(function() {
         var id = $(this).data('id');
         var estatus_id = $(this).data('idestatus');
-        var checkmarx = $('.checkmarx'+id).html();
-        var aprobado = $('.aprobado'+id).html();
-        var fuente = $('.fuente'+id).html();
-        var resuelto =$('.resuelto'+id).html();
-        var aprobados =$('.aprobados'+id).html();
-        var altas = $('.altas'+id).html();
-        var medias = $('.medias'+id).html();
-        var bajas = $('.bajas'+id).html();
-        var observacion = $('.observacion'+id).html();
+        var checkmarx = $('.checkmarx' + id).html();
+        var aprobado = $('.aprobado' + id).html();
+        var fuente = $('.fuente' + id).html();
+        var resuelto = $('.resuelto' + id).html();
+        var aprobados = $('.aprobados' + id).html();
+        var altas = $('.altas' + id).html();
+        var medias = $('.medias' + id).html();
+        var bajas = $('.bajas' + id).html();
+        var observacion = $('.observacion' + id).html();
 
         obj.idtd = id;
         obj.idestatus = estatus_id;
@@ -51,15 +54,15 @@ $(document).ready(function() {
         obj.accion = 'crearSelect';
         peticionAjax(obj);
 
-        $('.checkmarx'+id).html('<input style="width:100%" class="form-control checkmarxIN'+id+'" value='+checkmarx+'>');
-        $('.aprobado'+id).html('<input style="width:100%" class="form-control aprobadoIN'+id+'" value='+aprobado+'>');
-        $('.fuente'+id).html('<input style="width:100%" class="form-control fuenteIN'+id+'" value='+fuente+'>');
-        $('.resuelto'+id).html('<input style="width:100%" class="form-control resueltoIN'+id+'" value='+resuelto+'>');
-        $('.aprobados'+id).html('<input style="width:100%" class="form-control aprobadosIN'+id+'" value='+aprobados+'>');
-        $('.altas'+id).html('<input style="width:100%" class="form-control altasIN'+id+'" value='+altas+'>');
-        $('.medias'+id).html('<input style="width:100%" class="form-control mediasIN'+id+'" value='+medias+'>');
-        $('.bajas'+id).html('<input style="width:100%" class="form-control bajasIN'+id+'" value='+bajas+'>');
-        $('.observacion'+id).html('<input style="width:100%" class="form-control observacionIN'+id+'" value='+observacion+'>');
+        $('.checkmarx' + id).html('<input style="width:100%" class="form-control checkmarxIN' + id + '" value=' + checkmarx + '>');
+        $('.aprobado' + id).html('<input style="width:100%" class="form-control aprobadoIN' + id + '" value=' + aprobado + '>');
+        $('.fuente' + id).html('<input style="width:100%" class="form-control fuenteIN' + id + '" value=' + fuente + '>');
+        $('.resuelto' + id).html('<input type="number" min="0" style="width:100%" class="form-control resueltoIN' + id + '" value=' + resuelto + '>');
+        $('.aprobados' + id).html('<input type="number" min="0" style="width:100%" class="form-control aprobadosIN' + id + '" value=' + aprobados + '>');
+        $('.altas' + id).html('<input  type="number" min="0" style="width:100%" class="form-control altasIN' + id + '" value=' + altas + '>');
+        $('.medias' + id).html('<input type="number" min="0" style="width:100%" class="form-control mediasIN' + id + '" value=' + medias + '>');
+        $('.bajas' + id).html('<input type="number" min="0" style="width:100%" class="form-control bajasIN' + id + '" value=' + bajas + '>');
+        $('.observacion' + id).html('<input style="width:100%" class="form-control observacionIN' + id + '" value=' + observacion + '>');
 
         $('.editar_acciones_' + id).hide();
         $('.editar_acciones_cancelar' + id).removeAttr('style');
@@ -67,21 +70,31 @@ $(document).ready(function() {
 
     $('.save').click(function() {
         var id = $(this).data('id');
-        var selectEstatusId = $('.selectEstatus'+id).val();
-        var selectEstatusText = $('.opcionEstatus'+selectEstatusId).data('estatus');
-        var checkmarx = $('.checkmarxIN'+id).val();
-        var aprobado = $('.aprobadoIN'+id).val();
-        var fuente = $('.fuenteIN'+id).val();
-        var resuelto = $('.resueltoIN'+id).val();
-        var aprobados = $('.aprobadosIN'+id).val();
-        var altas = $('.altasIN'+id).val();
-        var medias = $('.mediasIN'+id).val();
-        var bajas = $('.bajasIN'+id).val();
-        var observacion = $('.observacionIN'+id).val();
+        var selectEstatusId = $('.selectEstatus' + id).val();
+        var selectEstatusText = $('.opcionEstatus' + selectEstatusId).data('estatus');
+        var checkmarx = $('.checkmarxIN' + id).val();
+        var aprobado = $('.aprobadoIN' + id).val();
+        var fuente = $('.fuenteIN' + id).val();
+        var resuelto = $('.resueltoIN' + id).val();
+        var aprobados = $('.aprobadosIN' + id).val();
+        var altas = $('.altasIN' + id).val();
+        var medias = $('.mediasIN' + id).val();
+        var bajas = $('.bajasIN' + id).val();
+        var observacion = $('.observacionIN' + id).val();
+        //modi
+        var totalPDC = $('.total' + id).val();
+
+        let totalRiesgos = parseInt(altas) + parseInt(bajas) + parseInt(medias);
+        alert(totalRiesgos + "/" + parseInt(totalPDC));
+
+        if ((totalVulnerabilidades) > (resuelto + aprobados)) {
+            console.log('el total de las');
+            alert('vulne' + totalVulnerabilidades);
+        }
 
         obj.url = '../sistema/updateSistema';
-        obj.data = { 
-            idSistema: id, 
+        obj.data = {
+            idSistema: id,
             idEstatus: selectEstatusId,
             checkmarx: checkmarx,
             aprobado: aprobado,
@@ -96,22 +109,22 @@ $(document).ready(function() {
         obj.type = 'POST';
         obj.accion = 'update';
 
-        $('.estatus'+id).html(selectEstatusText);
-        $('.checkmarx'+id).html(checkmarx);
-        $('.aprobado'+id).html(aprobado);
-        $('.fuente'+id).html(fuente);
-        $('.resuelto'+id).html(resuelto);
-        $('.aprobados'+id).html(aprobados);
-        $('.altas'+id).html(altas);
-        $('.medias'+id).html(medias);
-        $('.bajas'+id).html(bajas);
-        $('.observacion'+id).html(observacion);
+        $('.estatus' + id).html(selectEstatusText);
+        $('.checkmarx' + id).html(checkmarx);
+        $('.aprobado' + id).html(aprobado);
+        $('.fuente' + id).html(fuente);
+        $('.resuelto' + id).html(resuelto);
+        $('.aprobados' + id).html(aprobados);
+        $('.altas' + id).html(altas);
+        $('.medias' + id).html(medias);
+        $('.bajas' + id).html(bajas);
+        $('.observacion' + id).html(observacion);
         $('.editar_acciones_' + id).show();
         $('.editar_acciones_cancelar' + id).hide();
         peticionAjax(obj);
     });
 
-    $('.cancelar').click(function(){
+    $('.cancelar').click(function() {
         var id = $(this).data('id');
         var estatus = $(this).data('estatusnombre');
         var checkmarx = $(this).data('checkmarx');
@@ -124,16 +137,16 @@ $(document).ready(function() {
         var bajas = $(this).data('bajas');
         var observacion = $(this).data('observacion');
 
-        $('.estatus'+id).html(estatus);
-        $('.checkmarx'+id).html(checkmarx);
-        $('.aprobado'+id).html(aprobado);
-        $('.fuente'+id).html(fuente);
-        $('.resuelto'+id).html(resuelto);
-        $('.aprobados'+id).html(aprobados);
-        $('.altas'+id).html(altas);
-        $('.medias'+id).html(medias);
-        $('.bajas'+id).html(bajas);
-        $('.observacion'+id).html(observacion);
+        $('.estatus' + id).html(estatus);
+        $('.checkmarx' + id).html(checkmarx);
+        $('.aprobado' + id).html(aprobado);
+        $('.fuente' + id).html(fuente);
+        $('.resuelto' + id).html(resuelto);
+        $('.aprobados' + id).html(aprobados);
+        $('.altas' + id).html(altas);
+        $('.medias' + id).html(medias);
+        $('.bajas' + id).html(bajas);
+        $('.observacion' + id).html(observacion);
         $('.editar_acciones_' + id).show();
         $('.editar_acciones_cancelar' + id).hide();
     });
@@ -154,16 +167,16 @@ function peticionAjax(datos) {
                     break;
                 case "crearSelect":
                     var select = '';
-                    select += '<select class="form-select selectEstatus'+datos.idtd+'">';
-                    $.each(res.id, function(key,val){
-                        if(val == datos.idestatus){
-                            select += '<option value="'+val+'" class="opcionEstatus'+val+'" data-estatus="'+res.estatus[key]+'" selected>'+res.estatus[key]+'</option>'
+                    select += '<select class="form-select selectEstatus' + datos.idtd + '">';
+                    $.each(res.id, function(key, val) {
+                        if (val == datos.idestatus) {
+                            select += '<option value="' + val + '" class="opcionEstatus' + val + '" data-estatus="' + res.estatus[key] + '" selected>' + res.estatus[key] + '</option>'
                         } else {
-                            select += '<option value="'+val+'" class="opcionEstatus'+val+'" data-estatus="'+res.estatus[key]+'" >'+res.estatus[key]+'</option>'
+                            select += '<option value="' + val + '" class="opcionEstatus' + val + '" data-estatus="' + res.estatus[key] + '" >' + res.estatus[key] + '</option>'
                         }
                     });
                     select += '</select>'
-                    $('.estatus'+datos.idtd).html(select);
+                    $('.estatus' + datos.idtd).html(select);
                     break;
             }
         },
